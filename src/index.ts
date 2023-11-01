c19f67e44b0bc6f8dd2bbb02a7c8c5f32ec6da59
Ayim William <odhisayim@gmail.com>
2023-11-01 19:47:44 +0300

    done built in types until any

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,14 @@ let age: number = 20;
 if (age < 50 )
     age += 10;
 console.log(age);
+
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let isPublished = true; // would work the same as the one below because it is declared and initialised.
+// let isPublished: boolean = true;
+
+let level;
+// if not initialised the compiler assumes it is of type any thus we can reinitialise it 
+level = 1;
+level = 'a';
+// avoid using the any type 

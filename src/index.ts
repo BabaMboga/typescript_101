be7c71bb1515f8e8b3950afc6ab059c53806453c
Ayim William <odhisayim@gmail.com>
2023-11-06 16:25:25 +0300

    Finished on functions

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,13 @@ let user: [number, string] = [1, 'Mosh'];
 enum Size {Small =1 , Medium, Large};
 let mySize: Size = Size.Medium
 console.log(mySize)
+
+// exploring functions
+function calculateTax (income: number, taxYear=2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3
+}
+
+calculateTax(10_000, 2023)
+calculateTax(11_000)

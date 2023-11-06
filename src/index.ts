96efb230d74ea27cd70890744e0daf593689b4fa
Ayim William <odhisayim@gmail.com>
2023-11-07 02:38:26 +0300

    finished on optional chaining

--- src/index.ts
+++ src/index.ts
@@ -100,3 +100,29 @@ function greet(name : string | null | undefined) {
 }
 
 greet(undefined); // we are only able to use undefined or null because of the union operator in our parameters
+
+// optional chaining
+
+type Customer = {
+    birthday: Date
+};
+
+function getCustomer(id: number): Customer | null {
+    return id === 0 ? null : { birthday: new Date()};
+}
+
+let customer = getCustomer(0);
+// if (customer !== null && customer !== undefined)
+//you can avoid using the above if statement by using 
+// an optional property access operator
+console.log(customer?.birthday);
+
+let customer2 = getCustomer(1);
+console.log(customer?.birthday?.getFullYear());
+
+// optional element access operator
+//customers?.[0]
+
+//optional call
+let log: any = null;
+log?.('a');

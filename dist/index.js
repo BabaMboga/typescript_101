2c8aa543750b6d9fd0ab3ba1fcf228d3eaa17509
Ayim William <odhisayim@gmail.com>
2023-11-06 15:52:52 +0300

    Finished the any type

--- dist/index.js
+++ dist/index.js
@@ -3,4 +3,13 @@ let age = 20;
 if (age < 50)
     age += 10;
 console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let isPublished = true; // would work the same as the one below because it is declared and initialised.
+// let isPublished: boolean = true;
+let level;
+// if not initialised the compiler assumes it is of type any thus we can reinitialise it 
+level = 1;
+level = 'a';
+// avoid using the any type 
 //# sourceMappingURL=index.js.map
